6ce95a3930c1222d7af86e9c7b4e504fb9677543
kevin3238 <kevin3238@berkeley.edu>
2018-04-18 12:44:44 -0700

    login/signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions" }
+  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
