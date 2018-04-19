be269ec67f29fe8b40e77ea2cbf5bf8ab66d4743
kevin3238 <kevin3238@berkeley.edu>
2018-04-19 02:22:06 -0700

    added user controller and functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  devise_for :users, path: 'users', controllers: { sessions: "users/sessions" }
+  # get 'users/show'
+  # get 'home/index'
+  devise_for :users
+  # resources :users, only: [:show]
+  resources :users
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
